e351d4d647fff2bb9eb3a4d14bdf10a8272041ce
WaghAshu03 <waghashutosh03@gmail.com>
2023-01-04 12:06:50 +0530

    Created speed input for dvd logo in hamburger menu; change dvd logo color on collision

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function selectRandom(Arr) {
-  let randomVal = Math.floor(Math.random() * Arr.length);
+  let randomVal = Arr[Math.floor(Math.random() * Arr.length)];
   while (randomVal === 0) randomVal = Math.floor(Math.random() * Arr.length);
   return randomVal;
 }
@@ -7,14 +7,16 @@ function selectRandom(Arr) {
 let leftPos = 0;
 let topPos = window.innerHeight / 2;
 let leftMax = window.innerWidth - 100;
-let topMax = window.innerHeight - 59;
+let topMax = window.innerHeight - 50;
 let movingLeft = true;
 let movingDown = true;
-let movementSpeed = 1;
+let current = { left: movingLeft, down: movingDown };
+let movementSpeed = parseInt(document.getElementById("inputSpeed").value) / 5;
 
 setInterval(() => {
+  movementSpeed = parseInt(document.getElementById("inputSpeed").value) / 5;
   leftMax = window.innerWidth - 100;
-  topMax = window.innerHeight - 59;
+  topMax = window.innerHeight - 50;
 
   if (movingDown) topPos = topPos + movementSpeed;
   else topPos = topPos - movementSpeed;
@@ -29,20 +31,36 @@ setInterval(() => {
   }
   if (topPos > topMax) {
     movingDown = false;
+
     topPos =
       topPos - selectRandom([0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1]);
   }
   if (leftPos < 0) {
     movingLeft = true;
+
     leftPos =
       leftPos + selectRandom([0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1]);
   }
   if (leftPos > leftMax) {
     movingLeft = false;
+
     leftPos =
       leftPos - selectRandom([0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1]);
   }
 
+  if (current.left !== movingLeft || current.down !== movingDown) {
+    document.getElementById("dvdLogo").style.backgroundColor = selectRandom([
+      "#00f",
+      "#0ff",
+      "#f0f",
+      "#f00",
+      "#ff0",
+      "#0f0",
+      "#0ff",
+    ]);
+    current = { left: movingLeft, down: movingDown };
+  }
+
   document.getElementById("dvdLogo").style.top = `${topPos}px`;
   document.getElementById("dvdLogo").style.left = `${leftPos}px`;
-}, 10);
+}, 25);
